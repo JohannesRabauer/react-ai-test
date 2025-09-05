eed03a016808ae257096558d3216cf3e4eb6c28b
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-09-05 15:56:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ai-adventure-story/" // change to your repo name!
+  base: "/react-ai-test/" // change to your repo name!
 });
