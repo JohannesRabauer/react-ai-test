4d749679f13c45dfc971c84bd4e57e72be6de36d
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-09-05 16:39:06 +0200

    Fix #1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-ai-test/" // change to your repo name!
+  base: "/react-ai-test/"
 });
